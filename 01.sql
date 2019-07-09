d397dcbe1b5576b6cb57860b0cd6a620188ecc3a
Martin Gamboa <martinsillo.gamboa@gmail.com>
2019-07-09 15:00:48 -0500

    sql modificados

--- 01.sql
+++ 01.sql
@@ -1,56 +1,48 @@
-CREATE SCHEMA `seduzac_encuestas` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-USE `seduzac_encuestas`;
+CREATE SCHEMA `seduzac_encuestas_test` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE `seduzac_encuestas_test`;
+
 
 CREATE TABLE `configuracion` (
-  `id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
-  `tablet_num` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
-  `usuario` varchar(16) COLLATE utf8_unicode_ci NOT NULL,
-  `clave` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
-  `nombre` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
-  `email` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
-  `telefono` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-  
- CREATE TABLE `seduzac_encuestas`.`regiones` (
+  `id` TINYINT NOT NULL AUTO_INCREMENT,
+  `tablet_num` VARCHAR(32) NOT NULL,
+  `usuario` VARCHAR(16) NOT NULL,
+  `clave` VARCHAR(32) NOT NULL,
+  `nombre` VARCHAR(128) NOT NULL,
+  `email` VARCHAR(128) NOT NULL,
+  `telefono` VARCHAR(10) NOT NULL,
+   `registrado_web` TINYINT(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`));
+
+CREATE TABLE `regiones` (
   `id` TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
   `region` VARCHAR(64) NOT NULL,
   PRIMARY KEY (`id`));
   
-  CREATE TABLE `seduzac_encuestas`.`centros_trabajo` (
+ CREATE TABLE `centros_trabajo` (
   `id` SMALLINT NOT NULL AUTO_INCREMENT,	  
   `cct` VARCHAR(16) NOT NULL,
   `nombre` VARCHAR(254) NOT NULL,
   `region` TINYINT UNSIGNED NOT NULL,
   PRIMARY KEY (`id`));
-
-ALTER TABLE `seduzac_encuestas`.`centros_trabajo` 
-ADD INDEX `cct_region_idx` (`region` ASC);
-;
-ALTER TABLE `seduzac_encuestas`.`centros_trabajo` 
-ADD CONSTRAINT `cct_region`
-  FOREIGN KEY (`region`)
-  REFERENCES `seduzac_encuestas`.`regiones` (`id`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
   
-  CREATE TABLE `seduzac_encuestas`.`puestos` (
+    CREATE TABLE `puestos` (
   `id` SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
   `puesto` VARCHAR(64) NOT NULL,
   PRIMARY KEY (`id`));
   
-  CREATE TABLE `seduzac_encuestas`.`contratos` (
+
+   CREATE TABLE `contratos` (
   `id` SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
   `tipo_contrato` VARCHAR(32) NOT NULL,
   PRIMARY KEY (`id`));
   
-  CREATE TABLE `seduzac_encuestas`.`categorias` (
+  CREATE TABLE `categorias` (
   `id` SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
   `categoria` VARCHAR(32) NOT NULL,
   PRIMARY KEY (`id`));
   
-  CREATE TABLE `seduzac_encuestas`.`empleados` (
-  `id` SMALLINT NOT NULL AUTO_INCREMENT,	  
+    CREATE TABLE `empleados` (
+  `id` SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
   `rfc` VARCHAR(13) NOT NULL,
   `num_empleado` VARCHAR(45) NULL,
   `nombre` VARCHAR(254) NOT NULL,
@@ -61,59 +53,19 @@ ADD CONSTRAINT `cct_region`
   `cct` SMALLINT NOT NULL,
   PRIMARY KEY (`id`));
   
-  
-  
-  CREATE TABLE `encuestas` (
+    CREATE TABLE `encuestas` (
   `id` int(11) NOT NULL,
-  `folio` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `fecha` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `encuestador` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `empleado` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `q1` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `q2` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `q3` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `q4` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `q5` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `q6` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `q7` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `folio` varchar(45) not null,
+  `fecha` varchar(45) not null,
+  `encuestador` varchar(48) not null,
+  `empleado` smallint unsigned not null,
+  `q1` tinyint(1) unsigned not null,
+  `q2` text DEFAULT NULL,
+  `q3` DATE NOT NULL,
+  `q4` TEXT NOT NULL,
+  `q5` tinyint(1) unsigned,
+  `ineFront` MEDIUMTEXT,
+  `ineBack` MEDIUMTEXT,
+  `empleadoFoto` MEDIUMTEXT,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-  
-  -- Relaciones
-
-ALTER TABLE `seduzac_encuestas`.`empleados` 
-ADD INDEX `empleado_puesto_idx` (`puesto` ASC);
-;
-ALTER TABLE `seduzac_encuestas`.`empleados` 
-ADD CONSTRAINT `empleado_puesto`
-  FOREIGN KEY (`puesto`)
-  REFERENCES `seduzac_encuestas`.`puestos` (`id`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
-
-ALTER TABLE `seduzac_encuestas`.`empleados` 
-ADD INDEX `empleado_contrato_idx` (`tipo_contrato` ASC),
-ADD INDEX `empleado_categoria_idx` (`categoria` ASC);
-;
-ALTER TABLE `seduzac_encuestas`.`empleados` 
-ADD CONSTRAINT `empleado_contrato`
-  FOREIGN KEY (`tipo_contrato`)
-  REFERENCES `seduzac_encuestas`.`contratos` (`id`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION,
-ADD CONSTRAINT `empleado_categoria`
-  FOREIGN KEY (`categoria`)
-  REFERENCES `seduzac_encuestas`.`categorias` (`id`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
-  ALTER TABLE `seduzac_encuestas`.`empleados` 
-ADD INDEX `empleado_cct_idx` (`cct` ASC);
-;
-ALTER TABLE `seduzac_encuestas`.`empleados` 
-ADD CONSTRAINT `empleado_cct`
-  FOREIGN KEY (`cct`)
-  REFERENCES `seduzac_encuestas`.`centros_trabajo` (`id`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
-  
-  
--- 03.sql
+++ 03.sql
@@ -1,17 +1,17 @@
 -- creacion de usuario
-CREATE USER 'seduzac_consultas'@'localhost' IDENTIFIED BY 'F2AE9469B26199657C279E55B1747';
-CREATE USER 'seduzac_registros'@'localhost' IDENTIFIED BY '3C4F3B8B214AF57424728AEE374CD';
+CREATE USER 'seduzac_con'@'localhost' IDENTIFIED BY 'F2AE9469B26199657C279E55B1747';
+CREATE USER 'seduzac_reg'@'localhost' IDENTIFIED BY '3C4F3B8B214AF57424728AEE374CD';
 
 -- permisos de los usuarios
-GRANT SELECT ON seduzac_encuestas.* TO 'seduzac_consultas'@'localhost';
-GRANT SELECT ON seduzac_encuestas.* TO 'seduzac_registros'@'localhost';
-GRANT INSERT ON seduzac_encuestas.* TO 'seduzac_registros'@'localhost';
+GRANT SELECT ON seduzac_encuestas.* TO 'seduzac_con'@'localhost';
+GRANT SELECT ON seduzac_encuestas.* TO 'seduzac_reg'@'localhost';
+GRANT INSERT ON seduzac_encuestas.* TO 'seduzac_reg'@'localhost';
 
 -- permisos de Procedimientos almacenados
-GRANT EXECUTE ON PROCEDURE seduzac_encuestas.revisarConfiguracion TO 'seduzac_consultas'@'localhost';
-GRANT EXECUTE ON PROCEDURE seduzac_encuestas.instalacion TO 'seduzac_registros'@'localhost';
-GRANT EXECUTE ON PROCEDURE seduzac_encuestas.login TO 'seduzac_consultas'@'localhost';
-GRANT EXECUTE ON PROCEDURE seduzac_encuestas.infoConfig TO 'seduzac_consultas'@'localhost';
-GRANT EXECUTE ON PROCEDURE seduzac_encuestas.listaEmpleados TO 'seduzac_consultas'@'localhost';
+GRANT EXECUTE ON PROCEDURE seduzac_encuestas.revisarConfiguracion TO 'seduzac_con'@'localhost';
+GRANT EXECUTE ON PROCEDURE seduzac_encuestas.instalacion TO 'seduzac_reg'@'localhost';
+GRANT EXECUTE ON PROCEDURE seduzac_encuestas.login TO 'seduzac_con'@'localhost';
+GRANT EXECUTE ON PROCEDURE seduzac_encuestas.infoConfig TO 'seduzac_con'@'localhost';
+GRANT EXECUTE ON PROCEDURE seduzac_encuestas.listaEmpleados TO 'seduzac_con'@'localhost';
 FLUSH PRIVILEGES;
 
