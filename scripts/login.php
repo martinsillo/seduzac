dc9ed15ba7e548aaeb88d7d84f9a4d633a2310a6
Martin Gamboa <martinsillo.gamboa@gmail.com>
2019-07-10 15:13:42 -0500

    quitamos errores

--- scripts/login.php
+++ scripts/login.php
@@ -1,6 +1,6 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors',1);
+
+ini_set('display_errors',0);
 session_start();
 if($_SESSION['login'] == "si"){
         require_once('constantes.php');
